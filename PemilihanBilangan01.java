9698b2ee3fb40db10669dcae3d50221443424a1b
titaaknt <agnestitania3@gmail.com>
2024-09-24 17:30:26 +0700

    Add files via upload

--- PemilihanBilangan01.java
+++ PemilihanBilangan01.java
@@ -5,13 +5,9 @@ public class PemilihanBilangan01 {
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else 
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap " : " Angka " + angka + " termasuk bilangan ganjil ";
+
+        System.out.println(hasil);
     }
 }
